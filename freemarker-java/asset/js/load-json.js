6b60e1a593877c24685ee076cd71d799cc8a686f
Touhid Mia <hmtm.cse@gmail.com>
2020-07-13 02:52:03 +0600

    update

--- freemarker-java/asset/js/load-json.js
+++ freemarker-java/asset/js/load-json.js
@@ -20,12 +20,36 @@ function selectRow(row) {
     table.find(".row-" + row).addClass("table-success")
 }
 
-var playAllInterval;
+var playAllInterval, spellingPractiseInterval, counterInterval;
+
 
 function closePlayAll() {
     if (playAllInterval !== undefined){
         clearInterval(playAllInterval);
     }
+}
+
+function closeSpellingPractise() {
+    if (spellingPractiseInterval !== undefined){
+        clearInterval(spellingPractiseInterval);
+    }
+}
+
+function closeCounterInterval() {
+    if (counterInterval !== undefined){
+        clearInterval(counterInterval);
+    }
+    jQuery("#counter").html("")
+}
+
+function startCounterInterval(count) {
+    closeCounterInterval();
+    var counter = jQuery("#counter")
+    counter.html("")
+    counter.html(" (" + count + ")")
+    counterInterval = setInterval(function () {
+        counter.html(" (" + (count--) + ")")
+    }, 1000)
 }
 
 function playEnglishWord(wordList){
@@ -44,6 +68,103 @@ function playEnglishWord(wordList){
     }
 }
 
+var correct = 0, wrong = 0;
+
+function spellCheckAndAddRow(wordDetails) {
+    var spellInput = jQuery("#spell-input")
+    var value = spellInput.val()
+    spellInput.val("");
+    spellInput.focus();
+
+    var yesNo = "<button type=\"button\" class=\"btn btn-danger\">NO</button>"
+    if (wordDetails !== undefined && wordDetails.english !== undefined){
+        var areEqual = value.toLowerCase() === wordDetails.english.toLowerCase();
+        if (areEqual){
+            yesNo = "<button type=\"button\" class=\"btn btn-success\">YES</button>"
+            correct++
+        }else{
+            wrong++
+        }
+
+        var table = "<tr>";
+        table += "<td>" + yesNo + "</td>";
+        table += "<td>" + value + "</td>";
+        table += "<td>" + wordDetails.english + " ( " + wordDetails.bangla + " )</td>";
+        table += "</tr>";
+        jQuery("#spelling-practise-add-row").prepend(table);
+        jQuery("#wrong").text(" " + wrong)
+        jQuery("#correct").text(" " + correct)
+    }
+
+}
+
+function spellingPractise(wordList){
+    closeAll();
+    jQuery("#vocabulary-list").hide();
+    var spellingArea = jQuery("#spelling-list");
+    spellingArea.show();
+    spellingArea.html("")
+
+    var html = ""
+
+    html += "<div class=\"form-group\">"
+    html += "<label>Please Write the word, which you just listen. <span id='counter'></span></label>"
+    html += "<input type=\"text\" id='spell-input' class=\"form-control\">"
+    html += "</div><br/><div><strong>Total: <span id='total'> 0</span></strong>, <strong>Correct: <span id='correct'> 0</span></strong>, <strong>Wrong: <span id='wrong'> 0</span></strong></div><br/>"
+
+
+
+    html += "<table class=\"table table-condensed\">";
+    html += "<thead><tr>";
+    html += "<th>Is Correct</th>";
+    html += "<th>Your Spelling</th>";
+    html += "<th>Actual Spelling</th>";
+    html += "</tr></thead>";
+    html += "<tbody id='spelling-practise-add-row'>";
+    html += "</table>";
+    html += "</tbody>";
+    spellingArea.html(html)
+    jQuery("#spell-input").focus()
+    jQuery("#total").text(" " + (wordList.length - 1))
+    correct = 0
+    wrong = 0;
+
+
+    if (wordList !== undefined){
+        let index = 0, isFirst = true;
+        var totalSpan = 6;
+        startCounterInterval(totalSpan)
+        spellingPractiseInterval = setInterval(function () {
+            closeCounterInterval()
+            startCounterInterval(totalSpan)
+            if (!isFirst){
+                spellCheckAndAddRow(wordList[index])
+            }
+            index++;
+            if (wordList[index] !== undefined){
+                textToSpeech(wordList[index].english, function () {
+                    if (index === wordList.length){
+                        spellCheckAndAddRow(wordList[index]);
+                        closeCounterInterval();
+                    }
+                });
+            }
+
+            if (index > wordList.length){
+                closeSpellingPractise();
+                closeCounterInterval();
+            }
+            isFirst = false;
+        }, totalSpan * 1000)
+    }
+}
+
+function closeAll(){
+    closeCounterInterval();
+    closePlayAll();
+    closeSpellingPractise();
+}
+
 
 
 jQuery(document).ready(function () {
@@ -57,10 +178,10 @@ jQuery(document).ready(function () {
                     wordList = result;
                     var button = "<br/><div class=\"btn-group\" role=\"group\">";
                     button += "<button type=\"button\" class=\"btn btn-secondary play-word\">Play All</button>";
-                    button += "<button type=\"button\" class=\"btn btn-secondary\">Practise</button>";
+                    button += "<button type=\"button\" class=\"btn btn-secondary spelling-practise\">Practise</button>";
                     button += "</div><br/><br/>";
 
-                    var table = "<table class=\"table table-condensed\">";
+                    var table = "<div id='vocabulary-list'><table class=\"table table-condensed\">";
                     table += "<thead><tr>";
                     table += "<th>English</th>";
                     table += "<th>Bangla</th>";
@@ -80,12 +201,19 @@ jQuery(document).ready(function () {
 
 
                     table += "</table>";
-                    table += "</tbody>";
+                    table += "</tbody></div><div id='spelling-list'></div>";
                     loadData.html("");
                     loadData.html(button + table);
 
                     jQuery(".play-word").on( "click", function() {
+                        closeAll()
                         playEnglishWord(wordList);
+                        jQuery("#vocabulary-list").show();
+                        jQuery("#spelling-list").hide();
+                    });
+
+                    jQuery(".spelling-practise").on( "click", function() {
+                        spellingPractise(wordList);
                     });
 
                 }
